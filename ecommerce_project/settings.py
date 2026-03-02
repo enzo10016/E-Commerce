ea006bd38dd22dfb175905e1591b1316707ec568
enzo10016 <enzo10016100@gmail.com>
2026-03-02 18:08:43 +0800

    Fix 500: switch to fallback static storage and help deployment

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Use a more lenient storage that doesn't require a manifest file
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
