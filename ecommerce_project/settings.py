dd576a2b33da50c382af96b45eea4aa76b45b515
enzo10016 <enzo10016100@gmail.com>
2026-03-02 17:32:41 +0800

    Fix Render build: clean requirements and config env vars

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7-ywi_$^j_io)$d8023w2)nflftceu579jf5d-^)-$%cfc1rhy'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-7-ywi_$^j_io)$d8023w2)nflftceu579jf5d-^)-$%cfc1rhy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # In production, replace with your render url
 
 
 # Application definition
@@ -79,7 +83,7 @@ DATABASES = {
         'NAME': 'ecommerce_db',
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
-            'host': 'mongodb+srv://mebrosales_db_user:SXo83eEdd549OdIi@cluster0.lbnlwpz.mongodb.net/?appName=Cluster0',
+            'host': os.getenv('MONGO_URI', 'mongodb+srv://mebrosales_db_user:SXo83eEdd549OdIi@cluster0.lbnlwpz.mongodb.net/?appName=Cluster0'),
         }
     }
 }
